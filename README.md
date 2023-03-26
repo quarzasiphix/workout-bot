feb6549e233baa7445283ad2390ecdaabee76080
quarzasiphix <quarrysiphix@gmail.com>
2023-03-26 21:11:50 +0100

    made it work for every hour, now im gonna make it grab work outs from my backend

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 <h3> exercise bot </h3>
 <p> that calls webhook as a reminder to exercise, based on schedule </p>
 
+
 ![example](https://github.com/quarzasiphix/workout-bot/blob/master/example.png)
--- reminderbot.py
+++ reminderbot.py
@@ -2,14 +2,29 @@ import requests
 import schedule
 import time
 from datetime import datetime
+import pytz
 
 with open('webhook.txt', 'r') as f:
     WEBHOOK_URL = f.read().strip()
 ICON = "https://cdn.discordapp.com/attachments/1089456058507989023/1089456956281982986/images.png"
 NAME = "Workout Reminder"
 COLOR = 5526612
+# set the timezone to Warsaw
+tz = pytz.timezone('Europe/Warsaw')
+# get the current time in Warsaw
+now = datetime.now(tz)
+# format the current time as a string
+current_time = now.strftime("%H:%M:%S")
+current_hour = now.hour
+
+def update_time():
+    global now, current_time, current_hour
+    now = datetime.now(tz)
+    current_time = now.strftime("%H:%M:%S")
+    current_hour = now.hour
 
 def send_message(workout, message):
+    update_time()
     response = requests.post(WEBHOOK_URL, json={
     "username": "Reminder",
     "avatar_url": ICON,
@@ -48,24 +63,26 @@ def send_message(workout, message):
 message = "You've been a bum for an entire hour, its time to get up"
 
 def workout_reminder():
-    now = datetime.now()
-    current_hour = now.hour
+    print("reminder called")
     if current_hour == 10:
         send_message('Time for a workout!')
     elif current_hour == 11:
-        send_message(message + '\n do 30 push ups and some sit ups.')
+        send_message("push ups, sit ups", message + '\n do 30 push ups and some sit ups.')
     elif current_hour == 12:
-        send_message(message + '\n now its time for burpees, \n do 3 sets of 10 burpees ')
+        send_message("burpees", message + '\n now its time for burpees, \n do 3 sets of 10 burpees ')
+    elif current_hour == 22:
+        send_message("time", message + '\n test \n time: ' + current_time)
     else:
-        send_message('Time for a workout!')
+        send_message('test', 'Time for a workout!')
 
 # Schedule tasks to run every hour after 10am GMT
-schedule.every().day.at('10:00').do(workout_reminder)
-schedule.every().day.at('11:00').do(workout_reminder)
-schedule.every().day.at('12:00').do(workout_reminder)
+schedule.every(1).hour.do(workout_reminder)
 
 def start_reminder():
+    print("starting reminder")
+    print("current time: " + current_time)
     while True:
+        update_time()
         schedule.run_pending()
         time.sleep(1)
 
@@ -75,4 +92,4 @@ def main():
     
     #send_message(message + '\n now its time for burpees, \n do 3 sets of 10 burpees ')
 
-main()
+start_reminder()
