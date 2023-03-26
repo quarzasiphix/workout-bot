c008d795c9064cc28dc892f6ccf11b78e8797c02
quarzasiphix <quarrysiphix@gmail.com>
2023-03-26 09:23:37 +0100

    made it nicer

--- reminderbot.py
+++ reminderbot.py
@@ -7,22 +7,22 @@ from datetime import datetime
 WEBHOOK_URL = "https://discord.com/api/webhooks/1089454889089904690/kWg_Ef03R9ddL2U9hPBbcR9U10OOy2vTxldIXe_gXqMueQbaqJQZcsrCWdNyvOHh5Za6"
 ICON = "https://cdn.discordapp.com/attachments/1089456058507989023/1089456956281982986/images.png"
 NAME = "Workout Reminder"
-COLOR = 11119017
+COLOR = 5526612
 #BODY = 
 
 def send_message(workout, message):
     response = requests.post(WEBHOOK_URL, json={
-    "username": "Webhook",
-    "avatar_url": "https://i.imgur.com/4M34hi2.png",
+    "username": "Reminder",
+    "avatar_url": ICON,
     #"content": "Text message. Up to 2000 characters.",
     "embeds": [
         {
         "author": {
             "name": NAME,
-            "url": ICON,
-            "icon_url": ICON
+            #"url": ICON,
+            #"icon_url": ICON
         },
-        "title": "REMINDER",
+        #"title": "REMINDER",
         #"url": "https://google.com/",
         #"description": "Text message. You can use Markdown here. *Italic* **bold** __underline__ ~~strikeout~~ [hyperlink](https://google.com) `code`",
         "color": COLOR,
@@ -33,15 +33,9 @@ def send_message(workout, message):
                 "inline": True
             }
         ],
-        #"thumbnail": {
-        #    "url": "https://upload.wikimedia.org/wikipedia/commons/3/38/4-Nature-Wallpapers-2014-1_ukaavUI.jpg"
-        #},
-        #"image": {
-        #    "url": "https://upload.wikimedia.org/wikipedia/commons/5/5a/A_picture_from_China_every_day_108.jpg"
-        #},
         "footer": {
-            "text": "No one else is doing shit with their life, doing this will put you ahead 90%",
-            "icon_url": "https://i.imgur.com/fKL31aD.jpg"
+            "text": "You've been a bum for an entire hour, its time to get up",
+            "icon_url": ICON
         }
         }
     ]
@@ -78,7 +72,7 @@ def start_reminder():
 
 def main():
     print("calling send message")
-    send_message(message, '\n do 30 push ups and some sit ups.')
+    send_message("PUSH UPS", '\n do 30 push ups and some sit ups.')
     
     #send_message(message + '\n now its time for burpees, \n do 3 sets of 10 burpees ')
 
